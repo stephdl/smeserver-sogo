94ee3924c60b0d5d5bdb5e5bf91124b37c0ddc77
Stephane de Labrusse <stephdl@de-labrusse.fr>
2014-08-13 22:06:08 +0200

    wip

--- root/usr/share/perl5/vendor_perl/esmith/FormMagick/Panel/sogo.pm
+++ root/usr/share/perl5/vendor_perl/esmith/FormMagick/Panel/sogo.pm
@@ -2,7 +2,7 @@
 
 #----------------------------------------------------------------------
 # copyright (C) 2011 Firewall-Services
-# 
+#               2014 Stephane de Labrusse <stephdl@de-labrusse.fr>
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
